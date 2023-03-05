93f8a2ad498e67449ff3421b90457434eee31a23
Martins Nikiforovs 221RDB386 <126983656+221RDB386-Martins-Nikiforovs@users.noreply.github.com>
2023-03-05 18:41:06 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,15 +1,31 @@
-# python3
-
+# 221RDB386 Mārtiņš Nikiforovs 11.grupa
 import sys
 import threading
 import numpy
 
-
-def compute_height(n, parents):
+def compute_height(n, parentsList):
     # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
+    nodeChildren = [[] for _ in range(n)]
+    for i in range(n):
+      childParent = parentsList[i]
+      if childParent == -1:
+        treeRoot = i
+      else:
+        nodeChildren[childParent].append(i)
+
+
+  
+    def compute_depth(node):
+      if not nodeChildren[node]:
+        return 1
+      max_depth = 0
+      
+      for child in nodeChildren[node]:
+        depth = compute_depth(child)
+        max_depth = max(max_depth, depth)
+      return max_depth + 1
+    return compute_depth(treeRoot)
+
 
 
 def main():
@@ -21,7 +37,23 @@ def main():
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
-    pass
+    #pass
+  
+  keyInp = input()
+  if 'I' in keyInp:
+      n2 = int(input())
+      parentsList2 = list(map(int, input().split()))
+      heightI = compute_height(n2, parentsList2)
+      print(heightI)
+  elif 'F' in keyInp:
+    filename = input()
+    with open("test/" + filename,'r') as f:
+      n3 = int(f.readline())
+      parentsList3 = list(map(int, f.readline().split()))
+      heightF = compute_height(n3, parentsList3)
+      print(heightF)
+  else:
+    exit()
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
